9c0501880d91fd3bc1488bcbb4ace87bc2a9ded7
emras <eem5320@psu.edu>
2016-06-09 13:47:42 +0000

    Add files via upload

---
+++ StoreFront/Controllers/ShoppingCartController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Store_Front.Models;
+
+namespace Store_Front.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public ShoppingCartViewModel mdl = new ShoppingCartViewModel();
+        private StoreFrontEntities db = new StoreFrontEntities();
+
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            mdl.ShoppingCartItems = from p in db.spGetShoppingCartItems(mdl.UserID)
+                                    select p;
+            return View(mdl);
+        }
+
+
+
+        // GET: ShoppingCart/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
